e4d31812c410eafaaeaf51655a35664272f4c7b3
Naveed <naveedaslam782@gmail.com>
2017-09-28 10:25:12 +0500

    icon change

--- SmartSchoolManagementSystem/SmartSchoolManagementSystem/Program.cs
+++ SmartSchoolManagementSystem/SmartSchoolManagementSystem/Program.cs
@@ -16,7 +16,7 @@ namespace SmartSchoolManagementSystem
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Class_Section_Settings());
+            Application.Run(new Login());
         }
     }
 }

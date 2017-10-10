7e64821de7c72dd9a9f3ac77417b6312da460902
Naveed <naveedaslam782@gmail.com>
2017-10-10 11:58:12 +0500

    make new form vacation work

--- SmartSchoolManagementSystem/SmartSchoolManagementSystem/Program.cs
+++ SmartSchoolManagementSystem/SmartSchoolManagementSystem/Program.cs
@@ -17,7 +17,7 @@ namespace SmartSchoolManagementSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new ExpencesRecord());
+            Application.Run(new Login());
 
             
 

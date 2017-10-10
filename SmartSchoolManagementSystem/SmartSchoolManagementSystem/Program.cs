427e167e528b9e1c8dded92c633e3e4e08fc20fa
Naveed <naveedaslam782@gmail.com>
2017-10-10 15:12:58 +0500

    login

--- SmartSchoolManagementSystem/SmartSchoolManagementSystem/Program.cs
+++ SmartSchoolManagementSystem/SmartSchoolManagementSystem/Program.cs
@@ -17,7 +17,7 @@ namespace SmartSchoolManagementSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new MessageSender());
+            Application.Run(new Login());
 
             
 

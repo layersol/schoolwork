5496d2ce99ca45cd5d285168507f9e4ae3d7f695
Naveed <naveedaslam782@gmail.com>
2017-10-12 13:07:39 +0500

    add cuting lines

--- SmartSchoolManagementSystem/SmartSchoolManagementSystem/Program.cs
+++ SmartSchoolManagementSystem/SmartSchoolManagementSystem/Program.cs
@@ -17,7 +17,7 @@ namespace SmartSchoolManagementSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new StudentAccount());
+            Application.Run(new studentfeereciept());
 
             
 

4a3db14053b8dc65d575c3ed402c4d558666b0c4
Naveed <naveedaslam782@gmail.com>
2017-10-16 16:14:50 +0500

    add New user

--- SmartSchoolManagementSystem/SmartSchoolManagementSystem/CreateUser.cs
+++ SmartSchoolManagementSystem/SmartSchoolManagementSystem/CreateUser.cs
@@ -16,5 +16,10 @@ namespace SmartSchoolManagementSystem
         {
             InitializeComponent();
         }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

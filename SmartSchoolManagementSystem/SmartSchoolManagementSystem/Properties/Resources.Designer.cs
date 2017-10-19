55fd05bc5a942423a6b480479a7bbd0502b6f7f8
Naveed <naveedaslam782@gmail.com>
2017-10-19 16:02:54 +0500

    Benners

--- SmartSchoolManagementSystem/SmartSchoolManagementSystem/Properties/Resources.Designer.cs
+++ SmartSchoolManagementSystem/SmartSchoolManagementSystem/Properties/Resources.Designer.cs
@@ -533,6 +533,16 @@ namespace SmartSchoolManagementSystem.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap roll {
+            get {
+                object obj = ResourceManager.GetObject("roll", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap save {
             get {
                 object obj = ResourceManager.GetObject("save", resourceCulture);
